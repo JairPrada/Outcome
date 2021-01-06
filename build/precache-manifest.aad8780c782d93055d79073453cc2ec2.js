4da21732248d8183b78ca3e140de697b613309a8
JairPrada <JairPrada9@gmail.com>
2021-01-06 16:15:27 -0500

    subiendo a github pages

---
+++ build/precache-manifest.aad8780c782d93055d79073453cc2ec2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe6a9d6ad27ea813004575e1d1abfbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d12153be9f481790740",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "89ac8d79c2ca4b428276",
+    "url": "/static/css/main.880d1f19.chunk.css"
+  },
+  {
+    "revision": "2d12153be9f481790740",
+    "url": "/static/js/2.0151450c.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/static/js/2.0151450c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ac8d79c2ca4b428276",
+    "url": "/static/js/main.b7e2777b.chunk.js"
+  },
+  {
+    "revision": "f6dadad8f31ba7e075ad",
+    "url": "/static/js/runtime-main.f3d7ead6.js"
+  },
+  {
+    "revision": "03efc52a1398ab86811c219a565856c0",
+    "url": "/static/media/1.03efc52a.svg"
+  },
+  {
+    "revision": "227481d521665127848c72c55cdf372c",
+    "url": "/static/media/2.227481d5.svg"
+  },
+  {
+    "revision": "89661de327c02ec349ac7e8c3cc2cd35",
+    "url": "/static/media/3.89661de3.svg"
+  },
+  {
+    "revision": "51538cf6c364a15ed66960599f2adcde",
+    "url": "/static/media/Banner.51538cf6.mp4"
+  },
+  {
+    "revision": "c7259fe4e2c1cf3558264b33e1307b26",
+    "url": "/static/media/Google.c7259fe4.svg"
+  },
+  {
+    "revision": "057b522a08f2515a623abdd8086d8ab0",
+    "url": "/static/media/Outcome.057b522a.svg"
+  },
+  {
+    "revision": "bcb095352b98fb58ada29e95815308b4",
+    "url": "/static/media/Perfil.bcb09535.mp4"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
